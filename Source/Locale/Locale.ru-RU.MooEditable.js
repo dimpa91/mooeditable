9ca8cc5b395e27b78a44fb2dec83d876b3627d8d
dimpa91 <32429864+dimpa91@users.noreply.github.com>
2017-11-06 17:53:54 +0200

    Create Locale.ru-RU.MooEditable.js
    Russian locale. Maked from Locale.en-US.MooEditable.example.js

---
+++ Source/Locale/Locale.ru-RU.MooEditable.js
@@ -0,0 +1,124 @@
+/*
+---
+
+name: Locale.ru-RUS.MooEditable
+
+description: MooEditable messages for Russian.
+
+license: MIT-style license
+
+authors:
+- Kisenko Dmitry
+
+requires:
+- MooEditable
+- more:1.3/Locale
+
+provides: [Locale.ru-RU.MooEditable]
+
+...
+*/
+
+Locale.define('ru-RU', 'MooEditable', {
+	
+	// MooEditable
+	ok: 'OK',
+	cancel: 'Отмена',
+	bold: 'Жирный',
+	italic: 'Курсив',
+	underline: 'Подчеркнутый',
+	strikethrough: 'Перечеркнутый',
+	unorderedList: 'Ненумерованый список',
+	orderedList: 'Нумерованый список',
+	indent: 'Увеличить отступ',
+	outdent: 'Уменьшить отступ',
+	undo: 'Отменить',
+	redo: 'Вернуть',
+	removeHyperlink: 'Удалить ссылку',
+	addHyperlink: 'Добавить ссылку',
+	selectTextHyperlink: 'Выберите текст для ссылки',
+	enterURL: 'Введите URL',
+	enterImageURL: 'Введите URL изображения',
+	addImage: 'Добавить изображение',
+	toggleView: 'Переключить вид',
+	
+	// MooEditable.Charmap
+	insertCustomCharacter: 'Вставить свою главу',
+	insertCharacter: 'Вставить главу',
+	
+	// MooEditable.Extras
+	blockFormatting: 'Форматирование блоков',
+	paragraph: 'Параграф',
+	heading1: 'Заголовок 1',
+	heading2: 'Заголовок 2',
+	heading3: 'Заголовок 3',
+	alignLeft: 'Выравнивание по левому краю',
+	alignRight: 'Выравнивание по правому краю',
+	alignCenter: 'Выравнивание по центру',
+	alignJustify: 'Выравнивание по ширине',
+	removeFormatting: 'Удалить форматирование',
+	insertHorizontalRule: 'Добавить горизонтальное правило',
+	
+	// MooEditable.Flash
+	embed: 'Вставить код',
+	flashEmbed: 'Flash вставка',
+	
+	// MooEditable.Forecolor
+	changeColor: 'Изменить цвет',
+	
+	// MooEditable.Image
+	imageAlt: 'alt',
+	imageClass: 'класс',
+	imageAlign: 'выравнивание',
+	imageAlignNone: 'нет',
+	imageAlignLeft: 'лево',
+	imageAlignCenter: 'центр',
+	imageAlignRight: 'право',
+	addEditImage: 'Добавить/редактировать изображение',
+	
+	// MooEditable.Pagebreak
+	pageBreak: 'Разрыв страницы',
+	
+	// MooEditable.Smiley
+	insertSmiley: 'Вставить смайлик',
+	
+	// MooEditable.Table
+	tableColumns: 'колонки',
+	tableRows: 'строки',
+	tableWidth: 'ширина',
+	tableClass: 'cкласс',
+	tableType: 'тип',
+	tableHeader: 'Заголовок',
+	tableCell: 'ячейка',
+	tableAlign: 'выравнивание',
+	tableAlignNone: 'нет',
+	tableAlignCenter: 'центр',
+	tableAlignRight: 'право',
+	tableValign: 'вертикальное выравнивание',
+	tableValignNone: 'нет',
+	tableValignTop: 'по верху',
+	tableValignMiddle: 'по центру',
+	tableValignBottom: 'по низу',
+	addTable: 'Добавить таблицу',
+	editTable: 'Редактировать таблицу',
+	addTableRow: 'Добавить строку',
+	editTableRow: 'Редактировать строку',
+	mergeTableRow: 'Слияние строк',
+	splitTableRow: 'Разбить строку',
+	deleteTableRow: 'Удалить строку',
+	addTableCol: 'Добавить колонку',
+	editTableCol: 'Редактировать колонку',
+	mergeTableCell: 'Слияние ячеек',
+	splitTableCell: 'Разбить ячейку',
+	deleteTableCol: 'Удалить колонку',
+    
+    // MooEditable.UI.ExtendedLinksDialog
+    protocol: 'протокол',
+    link: 'ссылка',
+    email: 'e-Mail',
+    urlWithoutHttp: 'URL (без http://)',
+    window: 'окно',
+    sameWindow: 'это окно',
+    newWindow: 'новое окно'
+    
+});
